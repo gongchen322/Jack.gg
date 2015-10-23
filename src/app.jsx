2b036d5ee7e15b123436560c8dcefc8f6d08d7c3
gongchen322 <gong27@purdue.edu>
2015-10-23 03:10:39 -0400

    Nav-bar created

--- src/app.jsx
+++ src/app.jsx
@@ -2,16 +2,16 @@ var React = require('react');
 
 var Hello = React.createClass({
   render: function() {
-    return <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
+    return <nav className="navbar navbar-inverse">
+  <div className="container-fluid">
+    <div className="navbar-header">
+      <a className="navbar-brand" href="#">Jack.gg</a>
     </div>
     <div>
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
-          <ul class="dropdown-menu">
+      <ul className="nav navbar-nav">
+        <li className="active"><a href="#">Home</a></li>
+        <li className="dropdown"><a className="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span className="caret"></span></a>
+          <ul className="dropdown-menu">
             <li><a href="#">Page 1-1</a></li>
             <li><a href="#">Page 1-2</a></li>
             <li><a href="#">Page 1-3</a></li>
@@ -20,9 +20,9 @@ var Hello = React.createClass({
         <li><a href="#">Page 2</a></li>
         <li><a href="#">Page 3</a></li>
       </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      <ul className="nav navbar-nav navbar-right">
+        <li><a href="#"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
       </ul>
     </div>
   </div>
