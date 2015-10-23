44c8da30cc1810e43a6c951064b61f48cb353940
gongchen322 <chengong@Chens-MacBook-Pro-3.local>
2015-10-23 19:57:15 -0400

    background added

--- src/app.jsx
+++ src/app.jsx
@@ -2,41 +2,36 @@ var React = require('react');
 
 var Hello = React.createClass({
   render: function() {
+
     return <body>
     <div>
-    <nav className="navbar navbar-inverse">
-  <div className="container-fluid">
-    <div className="navbar-header">
-      <a id="a" className="navbar-brand" href="#">Jack.gg</a>
+      <nav className="navbar navbar-inverse">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a id="a" className="navbar-brand" href="#">Jack.gg</a>
+          </div>
+          <div>
+            <ul className="nav navbar-nav">
+              <li className="active"><a href="#">Home</a></li>
+              <li className="dropdown"><a className="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span className="caret"></span></a>
+                <ul className="dropdown-menu">
+                  <li><a href="#">Page 1-1</a></li>
+                  <li><a href="#">Page 1-2</a></li>
+                  <li><a href="#">Page 1-3</a></li>
+                </ul>
+              </li>
+              <li><a href="rune.html">Check Rune</a></li>
+              <li><a href="#">Page 3</a></li>
+            </ul>
+            <ul className="nav navbar-nav navbar-right">
+              <li><a href="#"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
+              <li><a href="#"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul>
+          </div>
+        </div> 
+      </nav>
     </div>
-    <div>
-      <ul className="nav navbar-nav">
-        <li className="active"><a href="#">Home</a></li>
-        <li className="dropdown"><a className="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span className="caret"></span></a>
-          <ul className="dropdown-menu">
-            <li><a href="#">Page 1-1</a></li>
-            <li><a href="#">Page 1-2</a></li>
-            <li><a href="#">Page 1-3</a></li>
-          </ul>
-        </li>
-        <li><a href="rune.html">Check Rune</a></li>
-        <li><a href="#">Page 3</a></li>
-      </ul>
-      <ul className="nav navbar-nav navbar-right">
-        <li><a href="#"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
-        <li><a href="#"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
-    </div>
-  </div> 
-</nav>
-
-<div id="example1">
-<h1>Lorem Ipsum Dolor</h1>
-<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
-<p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-</div>
-</div>
-</body>
+  </body>
   }
 });
 
