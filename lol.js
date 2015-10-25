0a080a410f48234eb3d0eae240d507afc5eee71c
gongchen322 <gong27@purdue.edu>
2015-10-25 05:20:35 -0400

    retrieved game info
    retrieved game information from a specific summoner name.

--- lol.js
+++ lol.js
@@ -1,6 +1,6 @@
 var sumName = "";
 var APIKEY = "";
-
+var sID="";
 function summonerLookUp() {   
     sumName = $("#summonerName").val();
     APIKEY = $("#APIKey").val();
@@ -22,19 +22,22 @@ function summonerLookUp() {
 
                 summonerLevel = json[sumNamenospace].summonerLevel;
                 summonerID = json[sumNamenospace].id;
-
+                
                 document.getElementById("sLevel").innerHTML = summonerLevel;
                 document.getElementById("sID").innerHTML = summonerID;
 
                 // NEW FUNCTION!
                 letsGetMasteries(summonerID);
+                gameLookUp(summonerID); 
 
             },
             error: function (XMLHttpRequest, textStatus, errorThrown) {
                 alert("error getting Summoner data 1!");
             }
         });
+        
     } else {}
+    
 }
 
 function letsGetMasteries(summonerID) {
@@ -57,4 +60,27 @@ function letsGetMasteries(summonerID) {
             alert("error getting Summoner data 2!");
         }
     });
+}
+
+function gameLookUp(summonerID) {   
+       
+        $.ajax({
+            url: 'https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/'+summonerID+'/recent?api_key='+APIKEY,
+            type: 'GET',
+            dataType: 'json',
+            data: {
+
+            },
+            success: function (json) {
+                
+
+                championID = json.games[0].championId;
+                document.getElementById("cID").innerHTML = championID;
+
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                alert("error getting Summoner data 1!");
+            }
+        });
+   
 }
